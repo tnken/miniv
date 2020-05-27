5b194f95d8ab646d6cae764ed655931972042f60
TakeruTanaka <takeru.tt5672@gmail.com>
2020-05-27 19:15:49 +0900

    fix parser test

--- parser/parser.v
+++ parser/parser.v
@@ -3,23 +3,35 @@ module parser
 import token
 
 enum NodeKind {
-  add  // +
-  sub  // -
-  mul  // *
-  div  // /
-  num  // number
-  lt   // <
-  le   // <=
-  eq   // ==
-  ne   // !=
+  add      // +
+  sub      // -
+  mul      // *
+  div      // /
+  num      // number
+  lt       // <
+  le       // <=
+  eq       // ==
+  ne       // !=
+  assign   // :=
+  lvar     // hoge
 }
 
 struct Node {
   pub mut:
   kind NodeKind
+  str string
   val int
   lhs &Node
   rhs &Node
+  offset int
+}
+
+pub fn (n Node) string() string {
+  if n.kind == .num {
+    return n.val.str()
+  } else {
+    return n.str
+  }
 }
 
 pub struct Parser {
@@ -31,20 +43,41 @@ pub fn new_parser(tk token.Token) &Parser {
   return &Parser{tk}
 }
 
-pub fn (p &Parser) parse() &Node {
-  return p.expr()
+pub fn (p &Parser) parse() []&Node {
+  return p.program()
 }
 
-fn new_node(kind NodeKind, lhs &Node, rhs &Node) &Node {
-  return &Node{kind, 0, lhs, rhs}
+fn new_node(kind NodeKind, s string, lhs &Node, rhs &Node) &Node {
+  return &Node{kind, s, 0, lhs, rhs, 0}
 }
 
 fn new_node_num(val int) &Node {
-  return &Node{.num, val, 0, 0}
+  return &Node{.num, '', val, 0, 0, 0}
+}
+
+fn (p &Parser) program() []&Node {
+  mut stmts := []&Node{}
+  for p.token.kind != .eof {
+    stmts << p.stmt()
+  }
+  return stmts
+}
+
+fn (p &Parser) stmt() &Node {
+  node := p.expr()
+  return node
 }
 
 fn (p &Parser) expr() &Node {
+  node := p.assign()
+  return node
+}
+
+fn (p &Parser) assign() &Node {
   node := p.equality()
+  if p.token.consume(':=') {
+    node = new_node(.assign, ':=', node, p.assign())
+  }
   return node
 }
 
@@ -53,9 +86,9 @@ fn (p &Parser) equality() &Node {
 
   for {
     if p.token.consume('==') {
-      node = new_node(.eq, node, p.relational())
+      node = new_node(.eq, '==', node, p.relational())
     } else if p.token.consume('!=') {
-      node = new_node(.ne, node, p.relational())
+      node = new_node(.ne, '!=', node, p.relational())
     } else {
       return node
     }
@@ -67,13 +100,13 @@ fn (p &Parser) relational() &Node {
 
   for {
     if p.token.consume('<') {
-      node = new_node(.lt, node, p.add())
+      node = new_node(.lt, '<', node, p.add())
     } else if p.token.consume('>'){
-      node = new_node(.lt, p.add(), node)
+      node = new_node(.lt, '>', p.add(), node)
       } else if p.token.consume('<=') {
-      node = new_node(.le, node, p.add())
+      node = new_node(.le, '<=', node, p.add())
     } else if p.token.consume('>=') {
-      node = new_node(.le, p.add(), node)
+      node = new_node(.le, '>=', p.add(), node)
     } else {
       return node
     }
@@ -85,9 +118,9 @@ fn (p &Parser) add() &Node {
 
   for {
     if p.token.consume('+') {
-      node = new_node(.add, node, p.mul())
+      node = new_node(.add, '+', node, p.mul())
     } else if p.token.consume('-') {
-      node = new_node(.sub, node, p.mul())
+      node = new_node(.sub, '-', node, p.mul())
     } else {
       return node
     }
@@ -99,9 +132,9 @@ fn (p &Parser) mul() &Node {
 
   for {
     if p.token.consume('*') {
-      node = new_node(.mul, node, p.unary())
+      node = new_node(.mul, '*', node, p.unary())
     } else if p.token.consume('/'){
-      node = new_node(.div, node, p.unary())
+      node = new_node(.div, '/', node, p.unary())
     } else {
       return node
     }
@@ -112,7 +145,7 @@ fn (p &Parser) unary() &Node {
   if p.token.consume('+'){
     return p.primary()
   } else if p.token.consume('-'){
-    return new_node(.sub, new_node_num(0), p.primary())
+    return new_node(.sub, '-', new_node_num(0), p.primary())
   }
 
   return p.primary()
@@ -122,6 +155,13 @@ fn (p &Parser) primary() &Node {
   if p.token.consume('(') {
     node := p.expr()
     p.token.expect(')')
+    return node
+  }
+
+  if p.token.kind == .ident {
+    node := &Node{.lvar, p.token.str, 0, 0, 0, 0}
+    node.offset = (int(p.token.str[0]) - int(`a`) + 1) * 8
+    p.token.next_token()
     return node
   }
 
